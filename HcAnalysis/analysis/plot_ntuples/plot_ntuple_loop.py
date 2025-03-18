55e55c469aecd5aa608644c08f2127075892a64c
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-03-18 15:13:38 +0100

    add preliminary analysis code

---
+++ HcAnalysis/analysis/plot_ntuples/plot_ntuple_loop.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+
+if __name__=='__main__':
+
+    configs = {
+      'event': {
+        'variables': 'variables/variables_event.json',
+        'genmatchbranch': None,
+        'outputdir': 'event',
+        'extrainfo': None
+      },
+      'event-gen': {
+        'variables': 'variables/variables_event_gen.json',
+        'genmatchbranch': None,
+        'outputdir': 'event',
+        'extrainfo': None
+      },
+      'Ds': {
+        'variables': 'variables/variables_ds.json',
+        'genmatchbranch': 'DsMeson_hasFastGenMatch',
+        'outputdir': 'Ds',
+        'extrainfo': r"'$D_{s} \rightarrow \phi \pi \rightarrow K K \pi$'"
+      },
+      'DStar': {
+        'variables': 'variables/variables_dstar.json',
+        'genmatchbranch': 'DStarMeson_hasFastGenMatch',
+        'outputdir': 'DStar',
+        'extrainfo': r"'$D* \rightarrow D^{0} \pi \rightarrow K \pi \pi$'"
+      }
+    }
+
+    inputfile = '../../python/output_20250317/output_test.root' # to replace with cmd line arg
+    outputdir = 'output_test' # to replace with cmd line arg
+
+    for key, config in configs.items():
+
+        variables = config['variables']
+        genmatchbranch = config['genmatchbranch']
+        thisoutputdir = os.path.join(outputdir, config['outputdir'])
+        extrainfo = config['extrainfo']
+
+        cmd = 'python3 plot_ntuple.py'
+        cmd += f' -i {inputfile}'
+        cmd += f' -v {variables}'
+        cmd += f' -o {thisoutputdir}'
+        cmd += ' --donormalize --dolog'
+        if extrainfo is not None: cmd += f' --extrainfo {extrainfo}'
+        if genmatchbranch is not None: cmd += f' --genmatchbranch {genmatchbranch}'
+        cmd += ' --extracmstext Simulation'
+
+        print(cmd)
+        os.system(cmd)
